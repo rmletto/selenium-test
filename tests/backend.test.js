4162f719954bb223ea7a7b4c538bf05cd429a57c
Ryan Letto <rmletto@mun.ca>
2020-11-29 20:47:41 -0330

    Fixed syntax error

--- tests/backend.test.js
+++ tests/backend.test.js
@@ -1,9 +1,5 @@
 const testBackend = require('./testBackend');
 
-beforeAll(() => {
-    return startServer();
-});
-
 test('Tests the /value endpoint', async () => {
     expect(await testBackend()).toBe("http://localhost:3000/value");
 });
