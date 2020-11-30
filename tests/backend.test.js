0fb308c5f2f3e99e61a47025981a89da06e02b56
Ryan Letto <rmletto@mun.ca>
2020-11-29 21:39:56 -0330

    More trial and error

--- tests/backend.test.js
+++ tests/backend.test.js
@@ -1,16 +1,18 @@
 const testBackend = require('./testBackend');
 const {Capabilities, Builder, By, Key, until} = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
 
 describe('back-end REST API testing example', () => {
     // Open the server and keep a reference to it
     const server = require('../index');
 
-    const chromeCapabilities = Capabilities.chrome();
     const chromeOptions = {
         'args': ['--no-sandbox', '--disable-dev-shm-usage', '--headless', '--remote-debugging-port=9222']
     };
-    chromeCapabilities.set('chromeOptions', chromeOptions);
-    const driver = new Builder().withCapabilities(chromeCapabilities).build();
+    const driver = new Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(new chrome.Options().headless())
+        .build();
 
     test('Tests the /value endpoint', async () => {
         expect(await testBackend(driver)).toBe('http://localhost:3000/value');
